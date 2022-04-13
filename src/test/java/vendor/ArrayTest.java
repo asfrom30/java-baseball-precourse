b44163474352d0e3c49361ed83c6ddd2fa953637
Doyoon Kim <asfrom30@gmail.com>
2022-04-14 08:51:27 +0900

    chore: 자바 기본적인 문법 확인(String, Array)

---
+++ src/test/java/vendor/ArrayTest.java
@@ -0,0 +1,16 @@
+package vendor;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+
+public class ArrayTest {
+
+    @Test
+    public void findIndex() {
+        String[] strs = {"1", "2", "3"};
+        int index = Arrays.binarySearch(strs, "1");
+        assertThat(index).isEqualTo(0);
+    }
+}
---
+++ src/test/java/vendor/StringTest.java
@@ -0,0 +1,20 @@
+package vendor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+
+public class StringTest {
+
+    @Test
+    public void split() {
+        String str = "123";
+        String[] strs = str.split("");
+        assertThat(strs).containsExactly("1", "2", "3");
+    }
+
+
+
+}
