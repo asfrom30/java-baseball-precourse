e9d968cc424d5fe7e72153b46b89788a351a0270
Doyoon Kim <asfrom30@gmail.com>
2022-04-14 19:56:05 +0900

    feat: 숫자를 추론하는 함수 새로 구현(반환값 변경)

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.Arrays;
+
 public class Game {
 
     private final String answer;
@@ -20,5 +22,24 @@ public class Game {
         } else {
             return "";
         }
+    }
+
+    String[] newInfer(String input) {
+        String[] answerStrs = this.answer.split("");
+        String[] inputStrs = input.split("");
+
+        String[] temp = new String[3];
+        for (int i = 0; i < inputStrs.length; i++) {
+            String str = inputStrs[i];
+            int index = Arrays.binarySearch(answerStrs, str);
+
+            if (0 <= index) {
+                if(i == index) temp[i] = "S";
+                else temp[i] = "B";
+            } else {
+                temp[i] = "F";
+            }
+        }
+        return temp;
     }
 }
