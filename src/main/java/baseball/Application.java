4acf77a019b927d0cc31bf32eeb909af132ed341
Doyoon Kim <asfrom30@gmail.com>
2022-04-14 20:25:35 +0900

    fix: 사용자 입력 시마다 새 게임을 생성하는 버그 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,16 +7,23 @@ import camp.nextstep.edu.missionutils.Randoms;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Application {
+
     public static void main(String[] args) {
+
+        // TODO: 해당 부분에 대한 테스트케이스가 필요함
+        String random = Util.makeRandom(3);
+        Game game = new Game(random);
 
         while (true) {
             String input = Console.readLine();
 
             if ("1234".equals(input)) throw new IllegalArgumentException();
             if ("2".equals(input)) break;
+            if ("1".equals(input)) {
+                random = Util.makeRandom(3);
+                game = new Game(random);
+            }
 
-            String random = Util.makeRandom(3);
-            Game game = new Game(random);
             String result = game.infer(input);
             System.out.println(result);
         }
