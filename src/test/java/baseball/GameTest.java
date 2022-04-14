8529c3d8508de6d2c60be97b435b821b0dde6d9f
Doyoon Kim <asfrom30@gmail.com>
2022-04-14 19:55:30 +0900

    test: 새로운 추론함수 테스트 케이스 생성

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -15,5 +15,29 @@ public class GameTest {
         String result = game.infer("135");
         assertThat(result).isEqualTo("3스트라이크");
     }
-    
+
+    @Test
+    @DisplayName("정답이 135이고, 135로 추론")
+    public void newInferA() {
+        Game game = new Game("135");
+        String[] result = game.newInfer("135");
+        assertThat(result).containsExactly("S", "S", "S");
+    }
+
+    @Test
+    @DisplayName("정답이 135이고, 136으로 추론")
+    public void newInferB() {
+        Game game = new Game("135");
+        String[] result = game.newInfer("136");
+        assertThat(result).containsExactly("S", "S", "F");
+    }
+
+    @Test
+    @DisplayName("정답이 135이고, 351로 추론")
+    public void newInferC() {
+        Game game = new Game("135");
+        String[] result = game.newInfer("351");
+        assertThat(result).containsExactly("B", "B", "B");
+    }
+
 }
