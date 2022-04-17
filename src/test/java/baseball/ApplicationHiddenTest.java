cb748508803aa25054af6673d5ffff3a7f10d02c
Doyoon Kim <asfrom30@gmail.com>
2022-04-17 10:48:57 +0900

    test: 게임을 다 맞추지 않았는데, 1 또는 2를 입력하는 테스트 케이스 추가

--- src/test/java/baseball/ApplicationHiddenTest.java
+++ src/test/java/baseball/ApplicationHiddenTest.java
@@ -1,25 +1,33 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import camp.nextstep.edu.missionutils.test.NsTest;
 
 class ApplicationHiddenTest extends NsTest {
 
     @Test
-    @DisplayName("게임을 끝내지 않았는데 2를 입력하면")
-    void 게임종료_후_재시작() {
+    @DisplayName("게임을 끝내지 않았는데 1를 입력하면")
+    void test1() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-
     }
+
+    @Test
+    @DisplayName("게임을 끝내지 않았는데 2를 입력하면")
+    void test2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
