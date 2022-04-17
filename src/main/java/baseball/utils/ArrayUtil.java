d3cf06784a34c96328ed7e0d223d9dff195165aa
Doyoon Kim <asfrom30@gmail.com>
2022-04-17 10:12:55 +0900

    chore: 쓰지 않는 메서드 정리

--- src/main/java/baseball/utils/ArrayUtil.java
+++ src/main/java/baseball/utils/ArrayUtil.java
@@ -12,23 +12,11 @@ public class ArrayUtil {
         public abstract boolean test(Object object);
     }
 
-
     public static boolean every(Object[] array, Equality equality) {
         for (int i = 0; i < array.length; i++) {
             if(!equality.test(array[i])) return false;
         }
         return true;
-    }
-
-    public static boolean every(String[] array, String target) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != target) return false;
-        }
-        return true;
-    }
-
-    public static int count(String[] array, String target) {
-        return (int) Arrays.stream(array).filter(str -> str == target).count();
     }
 
     public static int count(Object[] array, Equality equality) {
